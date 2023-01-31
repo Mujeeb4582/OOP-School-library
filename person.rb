da43463950dfb9b36655c4af29b313680992cb1b
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-01-31 18:57:36 +0500

    remove the argument error

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,13 +11,13 @@ class Person
 
   private
 
-  def of_age?
+  def is_of_age?
     @age >= 18
   end
 
   public
 
   def can_use_services?
-    of_age? || @parent_permission
+    is_of_age? || @parent_permission
   end
 end
