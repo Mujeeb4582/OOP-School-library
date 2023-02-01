8dd79a4fa5c198cb31f764aa7c935c114a39b7dc
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-01 20:00:49 +0500

    Remove the bugs

--- person.rb
+++ person.rb
@@ -1,19 +1,15 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
-  end
-end
+require './nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    super
   end
 
   def correct_name
@@ -30,35 +26,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-class Decorator < Nameable
-  attr_accessor :name
-
-  def initialize(nameable = 'Unknown')
-    super
-    @nameable = nameable
-  end
-
-  def correct_name
-    @name.correct_name
-  end
-end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name[0, 10]
-  end
-end
-
-p person = Person.new(22, 'maximilianus')
-puts person.correct_name
-p capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-p capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
