780fffb1616fc8d593269fde98aa0242067ac6db
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-03 09:20:52 +0500

    Add the relationship between classroom and student

--- student.rb
+++ student.rb
@@ -5,17 +5,14 @@ class Student < Person
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students << (self) unless classroom.students.include?(self)
   end
 
   def play_hoocky
     '¯(ツ)/¯'
   end
 end
-
-student = Student.new('six', 12, 'Mujeeb')
-p student.classroom
-p student.name
-p student.age
-p student.can_use_services?
-p student.play_hoocky
