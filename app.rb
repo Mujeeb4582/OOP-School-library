6fee149ed30e9e21a3c2d365d5e3ff33de1cd9e5
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-04 10:03:48 +0500

    remove the list option code

--- app.rb
+++ app.rb
@@ -10,55 +10,6 @@ class App
     @books = []
     @people = []
     @rentals = []
-    puts "Welcome to School library App!\n\n"
-  end
-
-  def run
-    loop do
-      puts '=' * 60
-      puts display_options
-      puts '=' * 60
-      print 'Your option: '
-      option = user_option
-      puts option == 'Invalid option' ? 'Invalid option, please type correct number!' : available_options(option)
-      # puts try
-    end
-  end
-
-  def display_options
-    'Please choose an option by entering a number between 1 to 7:
-    1 - List of all books
-    2 - List of all people
-    3 - Create a person
-    4 - Create a book
-    5 - Create a rental
-    6 - List of all rental for given id
-    7 - Exit'
-  end
-
-  def user_option
-    user_choice = gets.chomp.to_i
-    user_choice.positive? && user_choice <= 7 ? user_choice : 'Invalid option'
-  end
-
-  def available_options(user_choice)
-    case user_choice
-    when 1
-      list_books
-    when 2
-      list_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    else
-      puts 'Thank you for using this app!'
-      exit
-    end
   end
 
   # ========================== show list of books ================================
