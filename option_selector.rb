bdb24d50c9f62885aaaff972bb06f0099711d3ac
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-06 22:43:41 +0500

    Add the optionSelector class

---
+++ option_selector.rb
@@ -0,0 +1,21 @@
+class OptionSelector
+  def available_options(user_choice, library)
+    case user_choice
+    when 1
+      library.list_books
+    when 2
+      library.list_people
+    when 3
+      library.create_person
+    when 4
+      library.create_book
+    when 5
+      library.create_rental
+    when 6
+      library.list_rentals
+    else
+      puts 'Thank you for using this app!\n'
+      exit
+    end
+  end
+end
