67cd3a55cfca41ca7fbae1bdb66d2d7e01f3741f
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-10 16:44:24 +0500

    remove the linters error

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -1,8 +1,8 @@
 require './nameable'
 
-RSpec.describe Nameable do
-  describe "#correct_name" do
-    it "raises an error" do
+describe Nameable do
+  describe '#correct_name' do
+    it 'raises an error' do
       expect { subject.correct_name }.to raise_error(NotImplementedError)
     end
   end
